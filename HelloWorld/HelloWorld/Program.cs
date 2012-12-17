a71f514788fb1a9259ffcb4111e1cac278fe1960
shyakaster <shyakaster@gmail.com>
2012-12-17 18:37:32 +0300

    Test commit and push

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world for git testing");
+            Console.WriteLine("Hello world for git testing for real");
             Console.ReadLine();
         }
     }
